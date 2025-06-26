0fc59d50e48ea97b57fed19b55c871203bd52efa
yeonee <leenayeon@sookmyung.ac.kr>
2025-06-27 01:57:11 +0900

    setting: 프로젝트 기본 세팅

---
+++ MVVM-CleanArchitecture-Practice/MVVM-CleanArchitecture-Practice/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MVVM-CleanArchitecture-Practice
+//
+//  Created by 이나연 on 6/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("✅ ViewDidLoad 호출됨")
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
